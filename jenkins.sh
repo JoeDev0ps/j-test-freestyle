36eb1caa7edcbd74bc41bb3b99d68023169981e6
Joe <153092825+JoeDev0ps@users.noreply.github.com>
2024-08-01 16:23:17 +0100

    Create jenkins.sh
    added a jenkins shell script to run in job3 pipeline

---
+++ jenkins.sh
@@ -0,0 +1,5 @@
+echo "Here's some additional script messaging from our jenkins build"
+echo "Our directory looks like this"
+ls /var/lib/jenkins
+echo "Here are the jobs we've run:"
+ls /var/lib/jenkins/jobs
